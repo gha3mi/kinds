6bd309c03759115f7f9386ef828760ca26970ce3
Seyed Ali Ghasemi <info@gha3mi.com>
2023-12-22 21:35:56 +0100

    Use iso_fortran_env and add REAL32, REAL64 macros.

--- src/kinds.f90
+++ src/kinds.f90
@@ -1,7 +1,17 @@
 module kinds
 
+   use iso_fortran_env, only: real32, real64, int32
+
    implicit none
 
-   integer, parameter :: rk = kind(0.0d0)
+#if defined(REAL32)
+   integer, parameter :: rk = real32
+#elif defined(REAL64)
+   integer, parameter :: rk = real64
+#else
+   integer, parameter :: rk = real64
+#endif
+
+   integer, parameter :: ik = int32
 
 end module kinds
