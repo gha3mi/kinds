811cfba1a35f09eae9c1cc2d2e730a9f54f03c6f
Seyed Ali Ghasemi <info@gha3mi.com>
2024-01-02 22:43:40 +0100

    Add ik32, ik64, rk32, rk64 and INT32, INT64

--- src/kinds.f90
+++ src/kinds.f90
@@ -1,6 +1,6 @@
 module kinds
 
-   use iso_fortran_env, only: real32, real64, int32
+   use iso_fortran_env, only: real32, real64, int32, int64
 
    implicit none
 
@@ -12,6 +12,18 @@ module kinds
    integer, parameter :: rk = real64
 #endif
 
+#if defined(INT32)
    integer, parameter :: ik = int32
+#elif defined(INT64)
+   integer, parameter :: ik = int64
+#else
+   integer, parameter :: ik = int32
+#endif
+
+   integer, parameter :: rk32 = real32
+   integer, parameter :: rk64 = real64
+   
+   integer, parameter :: ik32 = int32
+   integer, parameter :: ik64 = int64
 
 end module kinds
